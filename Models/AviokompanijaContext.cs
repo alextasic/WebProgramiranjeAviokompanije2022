1c0ceba455877c6902465bbb6ca40d7a779c5d70
acatasic <96747833+acatasic@users.noreply.github.com>
2022-01-26 00:09:07 +0100

    Update AviokompanijaContext.cs

--- Models/AviokompanijaContext.cs
+++ Models/AviokompanijaContext.cs
@@ -7,7 +7,7 @@ namespace Aviokompanija.Models
        
         public DbSet<Destinacija> Destination{get;set;}
         public DbSet<ZakazanLet> DestinationAirplane{get;set;}
-        public DbSet<Aviocompany>Aviocompany{get;set;}
+        public DbSet<Aviocompany> Aviocompany{get;set;}
         public DbSet<Avion> Airplane{get;set;}
         public AviokompanijaContext(DbContextOptions options):base(options)
         {
